39bf655a15a8201f9d80794d354636f79cb3f7ba
LemonLicker <mrbluebear3@gmail.com>
2012-10-17 20:57:47 +0100

    Added a bsic Listener!

---
+++ SectionProtection/src/com/puffybugs/SectPro/Listeners/BlockListener.java
@@ -0,0 +1,25 @@
+package com.puffybugs.SectPro.Listeners;
+
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+import com.puffybugs.SectPro.SectPro;
+
+public class BlockListener implements Listener{
+	
+	@EventHandler
+	public void onBlockPlace(BlockPlaceEvent event) {
+		
+		Player player = event.getPlayer();
+		Block block = event.getBlockPlaced();
+		
+		if(block.getTypeId() == SectPro.itemId[0] && block.getData() == SectPro.itemId[1]) {
+			//Block is the type specified in the config!
+		}
+		
+	}
+	
+}
