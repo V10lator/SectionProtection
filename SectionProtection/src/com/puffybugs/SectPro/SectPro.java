da9aaa00ae6a7b70ba8c0d0cdcc43ffaddb02f0c
LemonLicker <mrbluebear3@gmail.com>
2012-10-17 21:25:35 +0100

    New config variables added Sorted onEnable into seperate methods for neatness Added more descriptions

--- SectionProtection/src/com/puffybugs/SectPro/SectPro.java
+++ SectionProtection/src/com/puffybugs/SectPro/SectPro.java
@@ -1,19 +1,40 @@
 package com.puffybugs.SectPro;
 
+import java.io.File;
+import java.io.InputStream;
+
 import org.bukkit.Server;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.puffybugs.SectPro.Listeners.BlockListener;
+
 public class SectPro extends JavaPlugin{
 
+	//Used for makeConfig() and getConfigValues()
+	FileConfiguration config;
+	File configFile;
+	
+	//Used in this class
 	Server serv; //The server, used for methods such as getLogger()
 	PluginDescriptionFile pdf; //plugin.yml, used for methods such as getName()
+	
+	//Config Values
+	public static Integer[] itemId;
+	public static boolean sneakOverride;
 	
 	@Override
 	public void onEnable() {
 		
 		serv = getServer();
 		pdf = getDescription();
+		
+		makeConfig();
+		getConfigValues();
+		
+		serv.getPluginManager().registerEvents(new BlockListener(), this);
 		
 		serv.getLogger().info(getName()+" version "+pdf.getVersion()+" has been enabled!");
 		
@@ -23,6 +44,42 @@ public class SectPro extends JavaPlugin{
 	public void onDisable() {
 		
 		serv.getLogger().info(getName()+" version "+pdf.getVersion()+" has been disabled!");
+		
+	}
+	
+	/** Makes the config
+	 * 
+	 */
+	private void makeConfig() {
+		
+		configFile = new File("plugins" + File.separator + pdf.getName() + File.separator + "config.yml");
+		config = YamlConfiguration.loadConfiguration(configFile);
+
+		InputStream defaultStream = this.getResource("config.yml");
+		if (defaultStream != null) {
+			YamlConfiguration defaultConfig = YamlConfiguration.loadConfiguration(defaultStream);
+			config.setDefaults(defaultConfig);
+		}
+
+		if (!configFile.exists()) {
+			getConfig().options().copyDefaults(true);
+			saveConfig();
+		}
+		
+	}
+	
+	/** Gets the config values and assigns them to variables
+	 * 
+	 */
+	private void getConfigValues() {
+		
+		//Gets the boundary blocks
+		String itemIdRaw = config.getString("Block-ID");
+		itemId[0] = Integer.valueOf(itemIdRaw.split("/")[0]);
+		itemId[1] = Integer.valueOf(itemIdRaw.split("/")[1]);
+		
+		//Gets if Sneak Override has effect
+		sneakOverride = config.getBoolean("Sneak-Override");
 		
 	}
 	
