e13d62ecf7d8ef5245a8552fe29396bce7e327af
V10lator <v10lator@myway.de>
2012-10-18 15:34:31 +0200

    Reworked config.

--- SectionProtection/src/com/puffybugs/SectPro/SectPro.java
+++ SectionProtection/src/com/puffybugs/SectPro/SectPro.java
@@ -1,21 +1,15 @@
 package com.puffybugs.SectPro;
 
 import java.io.File;
-import java.io.InputStream;
 
 import org.bukkit.Server;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.configuration.Configuration;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.puffybugs.SectPro.Listeners.BlockListener;
 
 public class SectPro extends JavaPlugin{
-
-	//Used for makeConfig() and getConfigValues()
-	FileConfiguration config;
-	File configFile;
 	
 	//Used in this class
 	Server serv; //The server, used for methods such as getLogger()
@@ -42,19 +36,9 @@ public class SectPro extends JavaPlugin{
 	 */
 	private void makeConfig() {
 		
-		configFile = new File("plugins" + File.separator + pdf.getName() + File.separator + "config.yml");
-		config = YamlConfiguration.loadConfiguration(configFile);
-
-		InputStream defaultStream = this.getResource("config.yml");
-		if (defaultStream != null) {
-			YamlConfiguration defaultConfig = YamlConfiguration.loadConfiguration(defaultStream);
-			config.setDefaults(defaultConfig);
-		}
-
-		if (!configFile.exists()) {
-			getConfig().options().copyDefaults(true);
-			saveConfig();
-		}
+		File f = new File(getDataFolder(), "config.yml");
+		if(!f.exists())
+			saveDefaultConfig();
 		
 	}
 	
@@ -63,10 +47,27 @@ public class SectPro extends JavaPlugin{
 	 */
 	private void getConfigValues() {
 		
+		Configuration config = getConfig();
+		
 		//Gets the boundary blocks
 		String itemIdRaw = config.getString("Block-ID");
-		itemId[0] = Integer.valueOf(itemIdRaw.split("/")[0]);
-		itemId[1] = Integer.valueOf(itemIdRaw.split("/")[1]);
+		String[] split = itemIdRaw.split("/");
+		try
+		{
+			itemId[0] = Integer.valueOf(split[0]);
+		}
+		catch(NumberFormatException e)
+		{
+			itemId[0] = 0;
+		}
+		try
+		{
+			itemId[1] = Integer.valueOf(split[1]);
+		}
+		catch(NumberFormatException e)
+		{
+			itemId[1] = 0;
+		}
 		
 		//Gets if Sneak Override has effect
 		sneakOverride = config.getBoolean("Sneak-Override");
